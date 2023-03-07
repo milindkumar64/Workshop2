abbc341ceefd99f32844142a8d4b5f668d8f69c6
milindkumar64 <milindgupta@gmail.com>
2023-03-07 09:20:48 +0530

    Add: storing all the numbers in to dictionary

--- printNumber.sh
+++ printNumber.sh
@@ -1,4 +1,4 @@
-echo "Welcome to PrintNumber program"
+{}echo "Welcome to PrintNumber program"
 # 1 2 4 7 9 10
 
 num1=1
@@ -8,6 +8,7 @@ num7=7
 num9=9
 num10=10
 
+declare -A number
 
 for (( i=1; i<=10; i++ ))
 do
@@ -16,23 +17,31 @@ case $i in
 
     $num1)
        echo $i
+       number[$i]="1"
      ;;
     $num2)
        echo $i
+       number[$i]="2"
      ;;
     $num4)
        echo $i
+       number[$i]="4"
      ;;
     $num7)
        echo $i
+       number[$i]="7"
      ;;
     $num9)
        echo $i
+      number[$i]="9"
      ;;
     $num10)
        echo $i
+      number[$i]="10"
 
      ;;
 esac
 
 done
+
+echo "Stored numbers in dictionary are :" ${number[1]} ${number[2]} ${number[4]} ${number[7]} ${number[9]} ${number[10]}
