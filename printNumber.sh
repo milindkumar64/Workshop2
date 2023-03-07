c32744d266f9706c6bd1853ea750d913e75a1740
milindkumar64 <milindgupta@gmail.com>
2023-03-07 09:06:15 +0530

    Add: we selected some numbers

--- printNumber.sh
+++ printNumber.sh
@@ -1,7 +1,38 @@
 echo "Welcome to PrintNumber program"
+# 1 2 4 7 9 10
+
+num1=1
+num2=2
+num4=4
+num7=7
+num9=9
+num10=10
+
 
 for (( i=1; i<=10; i++ ))
 do
-    echo "Numbers are :" $i
+
+case $i in
+
+    $num1)
+       echo $i
+     ;;
+    $num2)
+       echo $i
+     ;;
+    $num4)
+       echo $i
+     ;;
+    $num7)
+       echo $i
+     ;;
+    $num9)
+       echo $i
+     ;;
+    $num10)
+       echo $i
+
+     ;;
+esac
 
 done
